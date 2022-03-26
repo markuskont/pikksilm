b51a51493d757abfd03d0ca7521868128285f544
Markus Kont <alias013@gmail.com>
2022-03-26 23:46:40 +0200

    enrich: improve bucket testing

--- pkg/enrich/bucket_test.go
+++ pkg/enrich/bucket_test.go
@@ -60,3 +60,64 @@ func TestInsertCurrent(t *testing.T) {
 	assert.Equal(t, "1.2.3.4", b.Buckets[0].Data.(*WinlogData).NetworkEvents[0].SrcIP.String())
 	assert.Equal(t, "5.6.7.8", b.Buckets[0].Data.(*WinlogData).NetworkEvents[1].SrcIP.String())
 }
+
+func TestBucketExpand(t *testing.T) {
+	ts, err := time.Parse(time.RFC3339, "2021-12-04T11:10:45.736Z")
+	assert.Nil(t, err)
+	buckets, err := newBuckets(
+		bucketsConfig{
+			Count: 3,
+			Size:  5 * time.Second,
+			ContainerCreateFunc: func() any {
+				return &WinlogData{
+					NetworkEvents: make([]models.NetworkEntry, 0),
+				}
+			},
+		},
+	)
+	assert.Nil(t, err)
+
+	val1 := []models.NetworkEntry{
+		{SrcIP: net.IPv4(1, 1, 1, 1)},
+	}
+	assert.Nil(t, buckets.insert(func(b *Bucket) error {
+		b.Data.(*WinlogData).NetworkEvents = val1
+		return nil
+	}, ts))
+	val2 := []models.NetworkEntry{
+		{SrcIP: net.IPv4(1, 1, 2, 1)},
+	}
+	assert.Nil(t, buckets.insert(func(b *Bucket) error {
+		b.Data.(*WinlogData).NetworkEvents = val2
+		return nil
+	}, ts.Add(6*time.Second)))
+	val3 := []models.NetworkEntry{
+		{SrcIP: net.IPv4(1, 1, 3, 1)},
+	}
+	assert.Nil(t, buckets.insert(func(b *Bucket) error {
+		b.Data.(*WinlogData).NetworkEvents = val3
+		return nil
+	}, ts.Add(12*time.Second)))
+	val4 := []models.NetworkEntry{
+		{SrcIP: net.IPv4(1, 1, 4, 1)},
+	}
+	assert.Nil(t, buckets.insert(func(b *Bucket) error {
+		b.Data.(*WinlogData).NetworkEvents = val4
+		return nil
+	}, ts.Add(18*time.Second)))
+	val4expandded := append(val4, models.NetworkEntry{SrcIP: net.IPv4(1, 1, 4, 2)})
+	assert.Nil(t, buckets.insert(func(b *Bucket) error {
+		b.Data.(*WinlogData).NetworkEvents = val4expandded
+		return nil
+	}, ts.Add(18*time.Second)))
+
+	assert.Equal(t, 3, len(buckets.Buckets))
+	assert.Equal(t, val4expandded, buckets.Buckets[2].Data.(*WinlogData).NetworkEvents)
+
+	last := buckets.Buckets[2].Time
+
+	buckets.check(func(b *Bucket) error {
+		assert.Equal(t, val4expandded, b.Data.(*WinlogData).NetworkEvents)
+		return nil
+	}, 1*time.Second, last.Add(5*time.Second))
+}
