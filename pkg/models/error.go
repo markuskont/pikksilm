e68279ba297fcb8e72a2810200b5bb7c9c31f55b
Markus Kont <markus@stamus-networks.com>
2022-04-06 23:02:08 +0300

    models: add error type for non critical switch

---
+++ pkg/models/error.go
@@ -0,0 +1,12 @@
+package models
+
+import "fmt"
+
+type ErrInvalidEvent struct {
+	Key string
+	Raw Entry
+}
+
+func (e ErrInvalidEvent) Error() string {
+	return fmt.Sprintf("invalid key - %s", e.Key)
+}
