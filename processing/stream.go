cb84f64bfe136542a221e5f364231b13a1745751
Markus Kont <markus@stamus-networks.com>
2022-12-02 10:56:22 +0200

    processing: split sharding to separate file

--- processing/stream.go
+++ processing/stream.go
@@ -6,53 +6,8 @@ import (
 	"time"
 
 	"github.com/go-redis/redis/v8"
-	"github.com/markuskont/datamodels"
 	"github.com/sirupsen/logrus"
 )
-
-type MapHandlerFunc func(datamodels.Map)
-
-type DataMapShards struct {
-	Channels []chan datamodels.Map
-	Ctx      context.Context
-	Len      uint64
-}
-
-func (s *DataMapShards) Handler() MapHandlerFunc {
-	return func(m datamodels.Map) {
-		entityID, ok := m.GetString("process", "entity_id")
-		if ok {
-			select {
-			case s.Channels[balanceString(entityID, s.Len)] <- m:
-			case <-s.Ctx.Done():
-				return
-			}
-		}
-	}
-}
-
-func NewDataMapShards(ctx context.Context, workers int) (*DataMapShards, error) {
-	if workers < 1 {
-		return nil, errors.New("invalid worker count for shard init")
-	}
-	shards := make([]chan datamodels.Map, workers)
-	for i := range shards {
-		ch := make(chan datamodels.Map)
-		shards[i] = ch
-	}
-	return &DataMapShards{
-		Ctx:      ctx,
-		Channels: shards,
-		Len:      uint64(len(shards)),
-	}, nil
-}
-
-func (s *DataMapShards) Close() error {
-	for i := range s.Channels {
-		close(s.Channels[i])
-	}
-	return nil
-}
 
 func waitOnRedis(ctx context.Context, client *redis.Client, logger *logrus.Logger) error {
 	// wait until redis is up
